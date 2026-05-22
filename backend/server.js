f2bb50037657b62f0a6cad24e214aaf331f16305
Adnan <mohdadnan.code@gmail.com>
2026-05-22 16:24:18 +0530

    Fix SMTP transporter

--- backend/server.js
+++ backend/server.js
@@ -28,18 +28,16 @@ const PORT = process.env.PORT || 5000
 
 // Mail transporter
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false,
+  },
 })
 
 transporter.verify((error, success) => {
